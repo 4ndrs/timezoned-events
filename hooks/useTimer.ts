611efa09bec2731bfb601cf0882dc3f88df87a48
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-03-28 01:19:11 +0200

    Add useTimer custom hook

---
+++ hooks/useTimer.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+/**
+ * Returns the seconds showing how much is left until/has
+ * passed since timestamp (unix)
+ *
+ */
+const useTimer = (timestamp: number) => {
+  const [seconds, setSeconds] = useState(timeDifference(timestamp));
+
+  useEffect(() => {
+    const id = setInterval(() => setSeconds(timeDifference(timestamp)), 1000);
+
+    return () => clearInterval(id);
+  }, [timestamp]);
+
+  return seconds;
+};
+
+const timeDifference = (timestamp: number) =>
+  Math.trunc((timestamp - Date.now()) / 1000);
+
+export default useTimer;
