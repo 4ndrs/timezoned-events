0094fd663a53221f99f7a2a3e2b08feac10025d5
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-25 23:02:44 +0200

    Add Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { Inter } from "next/font/google";
+
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
@@ -6,9 +8,11 @@ import type { AppProps } from "next/app";
 const inter = Inter({ subsets: ["latin"] });
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <main className={inter.className}>
-    <Component {...pageProps} />
-  </main>
+  <ChakraProvider>
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  </ChakraProvider>
 );
 
 export default App;
