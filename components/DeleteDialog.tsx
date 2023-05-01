4e2dc62e7315b2f1171db11cffe8a232ca87bd7d
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-05-02 00:10:13 +0200

    Add delete confirmation dialog

---
+++ components/DeleteDialog.tsx
@@ -0,0 +1,63 @@
+import { useRef } from "react";
+
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Box,
+  Button,
+} from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
+
+type Props = {
+  isOpen: boolean;
+  onClose: (remove?: boolean) => void;
+  eventTitle: string;
+};
+
+const DeleteDialog = ({ isOpen, onClose, eventTitle }: Props) => {
+  const cancelRef = useRef<HTMLButtonElement>(null);
+
+  return (
+    <AlertDialog
+      leastDestructiveRef={cancelRef}
+      isOpen={isOpen}
+      onClose={() => onClose()}
+    >
+      <AlertDialogOverlay />
+      <AlertDialogContent borderTop="solid red 5px">
+        <Box
+          p="10px"
+          left="50%"
+          transform="translateX(-50%)"
+          top="-24px"
+          position="absolute"
+          backgroundColor="red"
+          borderRadius="full"
+        >
+          <DeleteIcon boxSize={6} color="white" />
+        </Box>
+        <AlertDialogHeader>Delete event</AlertDialogHeader>
+
+        <AlertDialogBody>
+          Are you sure you want to delete <strong>{eventTitle}</strong>? this
+          action cannot be undone
+        </AlertDialogBody>
+
+        <AlertDialogFooter>
+          <Button ref={cancelRef} onClick={() => onClose()} mr={3}>
+            Cancel
+          </Button>
+          <Button colorScheme="red" onClick={() => onClose(true)}>
+            Delete
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default DeleteDialog;
--- components/EventViewer.tsx
+++ components/EventViewer.tsx
@@ -4,10 +4,14 @@ import { EditIcon, DeleteIcon, LinkIcon } from "@chakra-ui/icons";
 import { Box, Button, Card, Heading, Link, Text } from "@chakra-ui/react";
 
 import TimeVisualizer from "./TimeVisualizer";
+import DeleteDialog from "./DeleteDialog";
 
 import { useEvents } from "@/context";
+import { useState } from "react";
 
 const EventViewer = () => {
+  const [deleteDialogIsOpen, setDeleteDialogIsOpen] = useState(false);
+
   const {
     dispatch,
     state: { events, selectedEventId },
@@ -23,68 +27,80 @@ const EventViewer = () => {
     );
   }
 
-  const handleDelete = () => {
-    dispatch({ type: "delete", id: event.id });
-    dispatch({ type: "updateSelectedEvent", id: "" });
+  const handleDelete = (remove?: boolean) => {
+    if (remove) {
+      dispatch({ type: "delete", id: event.id });
+      dispatch({ type: "updateSelectedEvent", id: "" });
+    }
+
+    setDeleteDialogIsOpen(false);
   };
 
   return (
-    <Card
-      p="45px 65px"
-      display="flex"
-      flexDirection="column"
-      alignItems="flex-start"
-      maxWidth="1113px"
-      ml="auto"
-      mr="auto"
-    >
-      <Box alignSelf="flex-end" display="flex" gap="21">
-        <Button rightIcon={<EditIcon />} variant="link" colorScheme="teal">
-          Edit
-        </Button>
-        <Button
-          rightIcon={<DeleteIcon />}
-          variant="link"
-          colorScheme="teal"
-          onClick={handleDelete}
-        >
-          Delete
-        </Button>
-      </Box>
-
-      <Heading color="gray.800" mt="60px" mb="25px">
-        <TimeVisualizer date={new Date(event.date + event.offset)} />{" "}
-        {event.title}
-      </Heading>
-
-      <Text color="gray.500">{event.description}</Text>
-
-      <Box
-        borderRadius="2xl"
-        overflow="hidden"
-        alignSelf="center"
-        mt="98px"
-        mb="133px"
+    <>
+      <Card
+        p="45px 65px"
+        display="flex"
+        flexDirection="column"
+        alignItems="flex-start"
+        maxWidth="1113px"
+        ml="auto"
+        mr="auto"
       >
-        {event.image && (
-          <Image
-            src={event.image}
-            alt="Image to hype up the mood"
-            style={{ objectFit: "cover", width: "240px", height: "240px" }}
-            width={240}
-            height={240}
-          />
-        )}
-      </Box>
+        <Box alignSelf="flex-end" display="flex" gap="21">
+          <Button rightIcon={<EditIcon />} variant="link" colorScheme="teal">
+            Edit
+          </Button>
+          <Button
+            rightIcon={<DeleteIcon />}
+            variant="link"
+            colorScheme="teal"
+            onClick={() => setDeleteDialogIsOpen(true)}
+          >
+            Delete
+          </Button>
+        </Box>
 
-      <Box display="flex" gap="18">
-        {event.links?.map((link) => (
-          <Link key={link.title} isExternal color="teal.500" href={link.url}>
-            {link.title} <LinkIcon />
-          </Link>
-        ))}
-      </Box>
-    </Card>
+        <Heading color="gray.800" mt="60px" mb="25px">
+          <TimeVisualizer date={new Date(event.date + event.offset)} />{" "}
+          {event.title}
+        </Heading>
+
+        <Text color="gray.500">{event.description}</Text>
+
+        <Box
+          borderRadius="2xl"
+          overflow="hidden"
+          alignSelf="center"
+          mt="98px"
+          mb="133px"
+        >
+          {event.image && (
+            <Image
+              src={event.image}
+              alt="Image to hype up the mood"
+              style={{ objectFit: "cover", width: "240px", height: "240px" }}
+              width={240}
+              height={240}
+            />
+          )}
+        </Box>
+
+        <Box display="flex" gap="18">
+          {event.links?.map((link) => (
+            <Link key={link.title} isExternal color="teal.500" href={link.url}>
+              {link.title} <LinkIcon />
+            </Link>
+          ))}
+        </Box>
+      </Card>
+
+      <DeleteDialog
+        isOpen={deleteDialogIsOpen}
+        onClose={handleDelete}
+        eventTitle={event.title}
+      />
+    </>
   );
 };
 
