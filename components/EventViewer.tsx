5c0bdd96cb2496dc45f25b9335271a974bf1ba19
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-05-01 19:44:39 +0200

    Use the events context in EventViewer

--- components/EventViewer.tsx
+++ components/EventViewer.tsx
@@ -1,66 +1,80 @@
 import Image from "next/image";
+
 import { EditIcon, DeleteIcon, LinkIcon } from "@chakra-ui/icons";
 import { Box, Button, Card, Heading, Link, Text } from "@chakra-ui/react";
 
-import lumineHype from "@/assets/images/lumine-hype.webp";
+import TimeVisualizer from "./TimeVisualizer";
 
-const EventViewer = () => (
-  <Card
-    p="45px 65px"
-    display="flex"
-    flexDirection="column"
-    alignItems="flex-start"
-    maxWidth="1113px"
-    ml="auto"
-    mr="auto"
-  >
-    <Box alignSelf="flex-end" display="flex" gap="21">
-      <Button rightIcon={<EditIcon />} variant="link" colorScheme="teal">
-        Edit
-      </Button>
-      <Button rightIcon={<DeleteIcon />} variant="link" colorScheme="teal">
-        Delete
-      </Button>
-    </Box>
+import { useEvents } from "@/context";
 
-    <Heading color="gray.800" mt="60px" mb="25px">
-      2 days 3 hours 23 minutes 10 seconds
-      <br />
-      Until Genshin Stream
-    </Heading>
+const EventViewer = () => {
+  const {
+    state: { events, selectedEventId },
+  } = useEvents();
 
-    <Text color="gray.500">
-      The stream is coming! new version with new playable characters, let&apos;s
-      goooooooooooooo!
-    </Text>
+  const event = events.find((event) => event.id === selectedEventId);
 
-    <Box
-      w="240px"
-      borderRadius="2xl"
-      overflow="hidden"
-      alignSelf="center"
-      mt="98px"
-      mb="133px"
+  if (typeof event === "undefined") {
+    return (
+      <Box textAlign="center">
+        <Heading>No Events</Heading>
+      </Box>
+    );
+  }
+
+  return (
+    <Card
+      p="45px 65px"
+      display="flex"
+      flexDirection="column"
+      alignItems="flex-start"
+      maxWidth="1113px"
+      ml="auto"
+      mr="auto"
     >
-      <Image
-        src={lumineHype}
-        alt="A picture of Lumine hyped with the hands up"
-        placeholder="blur"
-      />
-    </Box>
+      <Box alignSelf="flex-end" display="flex" gap="21">
+        <Button rightIcon={<EditIcon />} variant="link" colorScheme="teal">
+          Edit
+        </Button>
+        <Button rightIcon={<DeleteIcon />} variant="link" colorScheme="teal">
+          Delete
+        </Button>
+      </Box>
 
-    <Box display="flex" gap="18">
-      <Link isExternal color="teal.500" href="#">
-        Bilibili <LinkIcon />
-      </Link>
-      <Link isExternal color="teal.500" href="#">
-        YouTube <LinkIcon />
-      </Link>
-      <Link isExternal color="teal.500" href="#">
-        Twitch <LinkIcon />
-      </Link>
-    </Box>
-  </Card>
-);
+      <Heading color="gray.800" mt="60px" mb="25px">
+        <TimeVisualizer date={new Date(event.date + event.offset)} />{" "}
+        {event.title}
+      </Heading>
+
+      <Text color="gray.500">{event.description}</Text>
+
+      <Box
+        borderRadius="2xl"
+        overflow="hidden"
+        alignSelf="center"
+        mt="98px"
+        mb="133px"
+      >
+        {event.image && (
+          <Image
+            src={event.image}
+            alt="Image to hype up the mood"
+            style={{ objectFit: "cover", width: "240px", height: "240px" }}
+            width={240}
+            height={240}
+          />
+        )}
+      </Box>
+
+      <Box display="flex" gap="18">
+        {event.links?.map((link) => (
+          <Link key={link.title} isExternal color="teal.500" href={link.url}>
+            {link.title} <LinkIcon />
+          </Link>
+        ))}
+      </Box>
+    </Card>
+  );
+};
 
 export default EventViewer;
