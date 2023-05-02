db486eaa3685ca9f03aab3a9652da2171b345db0
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-05-02 20:30:15 +0200

    Add local storage support

--- context/events.tsx
+++ context/events.tsx
@@ -1,4 +1,13 @@
-import { createContext, useContext, useReducer } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useRef,
+} from "react";
+
+import { UTC_OFFSETS } from "@/lib/offsets";
+import { z } from "zod";
 
 import type { TimezonedEvent } from "@/interfaces";
 
@@ -6,10 +15,23 @@ type Action =
   | { type: "add"; payload: TimezonedEvent }
   | { type: "delete"; id: string }
   | { type: "edit"; payload: TimezonedEvent }
+  | { type: "setAll"; payload: TimezonedEvent[] }
   | { type: "updateSelectedEvent"; id: string };
 
 type State = { events: TimezonedEvent[]; selectedEventId?: string };
 type Dispatch = (action: Action) => void;
+
+const StoredEventsSchema = z.array(
+  z.object({
+    id: z.string(),
+    title: z.string(),
+    date: z.string(),
+    offset: z.enum(UTC_OFFSETS),
+    description: z.string().optional(),
+    links: z.array(z.object({ title: z.string(), url: z.string() })),
+    image: z.string().optional(),
+  })
+);
 
 const eventsReducer = (state: State, action: Action) => {
   switch (action.type) {
@@ -34,6 +56,9 @@ const eventsReducer = (state: State, action: Action) => {
         }),
       };
 
+    case "setAll":
+      return { ...state, events: action.payload };
+
     case "updateSelectedEvent":
       return { ...state, selectedEventId: action.id };
 
@@ -42,16 +67,56 @@ const eventsReducer = (state: State, action: Action) => {
   }
 };
 
-const assertNever = (value: never) => {
-  throw new Error(`Unhanlded case: `, value);
-};
-
 const EventsContext = createContext<
   { state: State; dispatch: Dispatch } | undefined
 >(undefined);
 
+const eventsInitializer = () => {
+  const initialEvents: TimezonedEvent[] = [];
+
+  const storedEvents = localStorage.getItem("TimezonedEvents");
+
+  if (!storedEvents) {
+    return initialEvents;
+  }
+
+  const parsed = StoredEventsSchema.safeParse(JSON.parse(storedEvents));
+
+  if (!parsed.success) {
+    console.error(
+      "Failed parsing the events stored in the local storage\n",
+      parsed.error.message
+    );
+
+    return initialEvents;
+  }
+
+  return parsed.data;
+};
+
+const assertNever = (value: never) => {
+  throw new Error(`Unhanlded case: `, value);
+};
+
 const EventsProvider = ({ children }: { children: React.ReactNode }) => {
   const [state, dispatch] = useReducer(eventsReducer, { events: [] });
+
+  useEffect(() => {
+    const initialEvents = eventsInitializer();
+
+    dispatch({ type: "setAll", payload: initialEvents });
+  }, []);
+
+  const initialRender = useRef(true);
+
+  useEffect(() => {
+    if (initialRender.current) {
+      initialRender.current = false;
+      return;
+    }
+
+    localStorage.setItem("TimezonedEvents", JSON.stringify(state.events));
+  }, [state.events]);
 
   const value = { state, dispatch };
 
